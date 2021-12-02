ae6cb10a1468013b7bade6663e26170a2e378f72
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2021-12-02 17:55:48 -0600

    Random Hex Generator

---
+++ PROTOGEN.R
@@ -0,0 +1,25 @@
+hex <- function(n) {
+    n <- n %% 256
+    p1 <- n %/% 16
+    p2 <- n %% 16
+    c1 <- paste(p1)
+    c2 <- paste(p2)
+    
+    if (p1 >= 10) c1 <- intToUtf8(p1 - 10 + 65)
+    if (p2 >= 10) c2 <- intToUtf8(p2 - 10 + 65)
+    
+    return(paste(c1, c2, sep = ""))
+}
+
+protogen <- function() {
+    rgb <- floor(runif(3, 0, 256))
+    return(paste("\U0023", hex(rgb[1]), hex(rgb[2]), hex(rgb[3]), sep = ""))
+}
+
+for (x in 1 : 16) print(protogen())
+
+
+
+
+
+
