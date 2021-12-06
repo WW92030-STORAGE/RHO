0fe7d6745d1fa03fe798349de83dc3128cceb4c3
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2021-12-05 19:18:32 -0600

    RGB - Hex converter

--- PROTOGEN.R
+++ PROTOGEN.R
@@ -1,4 +1,4 @@
-hex <- function(n) { // Integer to hex string
+hex <- function(n) { # Integer to hex string
     n <- n %% 256
     p1 <- n %/% 16
     p2 <- n %% 16
@@ -11,13 +11,17 @@ hex <- function(n) { // Integer to hex string
     return(paste(c1, c2, sep = ""))
 }
 
-primagen <- function() { // Generate an array of 3 random byte integers
+primagen <- function() { # Generate an array of 3 random byte integers
     return(floor(runif(3, 0, 255)))
 }
 
-protogen <- function() { // Generate a random hex color
+protogen <- function() { # Generate a random hex color
     rgb <- floor(runif(3, 0, 256))
     return(paste("\U0023", hex(rgb[1]), hex(rgb[2]), hex(rgb[3]), sep = ""))
+}
+
+toString <- function(rgb) { # Convert RGB array into hex string
+  return(paste("\U0023", hex(rgb[1]), hex(rgb[2]), hex(rgb[3]), sep = ""))
 }
 
 for (x in 1 : 16) print(protogen())
